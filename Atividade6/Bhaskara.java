641463ad3fb203ef9a89b3c920442785ddb754bc
Jose Almir <joseamfj@outlook.com>
2023-03-13 14:31:50 -0300

    Create Bhaskare class

--- Atividade6/Bhaskara.java
+++ Atividade6/Bhaskara.java
@@ -5,5 +5,10 @@ public class Bhaskara {
     private double b;
     private double c;
 
+    public Bhaskara(int _a, int _b, int _c) {
+        this.a = _a;
+        this.b = _b;
+        this.c = _c;
+    }
     
 }
