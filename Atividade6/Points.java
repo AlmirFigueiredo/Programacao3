a26676d10a7662428ba631a42749b68cf97801eb
Jose Almir <joseamfj@outlook.com>
2023-03-13 15:06:42 -0300

    Create method to calculate distances among the points

--- Atividade6/Points.java
+++ Atividade6/Points.java
@@ -1,18 +1,27 @@
 package Atividade6;
 
-public class Pontos {
+public class Points {
     private double x1;
     private double y1;
     private double x2;
     private double y2;
+    private double distance;
 
-    public Pontos(int a, int b, int c, int y1, int y2) {
+
+
+    public Points(int a, int b, int c, int y1, int y2) {
         Bhaskara calculateRoots = new Bhaskara(a, b, c);
         this.x1 = calculateRoots.getR1();
         this.x2 = calculateRoots.getR2();
         this.y1 = y1;
         this.y2 = y2;
+        this.distance = calculateDistance();
     }
+    private double calculateDistance() {
+        return Math.sqrt(Math.pow((this.x2 - this.x1),2) + Math.pow((this.y2 - this.y1), 2));
+    }
+
+
     
 
 }
