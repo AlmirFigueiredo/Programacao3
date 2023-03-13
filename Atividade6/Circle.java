20adf9f555329bac39e4af7e1d426b937c0e8c26
Jose Almir <joseamfj@outlook.com>
2023-03-13 16:23:30 -0300

    Create Circle fields and constructor

--- Atividade6/Circle.java
+++ Atividade6/Circle.java
@@ -1,5 +1,14 @@
 package Atividade6;
 
 public class Circle {
+    private double xc;
+    private double yc;
+    private double r;
+
+    public Circle(double xc, double yc, double r) {
+        this.xc = xc;
+        this.yc = yc;
+        this.r = r;
+    }
     
 }
