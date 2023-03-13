4227d33b490c6302426ee9e57613d2fab5674aef
Jose Almir <joseamfj@outlook.com>
2023-03-13 14:40:44 -0300

    Create method to calculate delta, other one to calculate r1 and another one to calculate r2

--- Atividade6/Bhaskara.java
+++ Atividade6/Bhaskara.java
@@ -4,11 +4,28 @@ public class Bhaskara {
     private double a;
     private double b;
     private double c;
+    private double delta;
+    private double r1;
+    private double r2;
 
     public Bhaskara(int _a, int _b, int _c) {
         this.a = _a;
         this.b = _b;
         this.c = _c;
+        calculateDelta();
+        calculateR1();
+        calculateR2();
     }
-    
+    private void calculateDelta() {
+        this.delta = (Math.pow(this.b, 2) - 4*(this.a * this.c));
+    }
+    private void calculateR1() {
+        this.r1 = ((-this.b - Math.sqrt(this.delta))/2*this.a);
+    }
+    private void calculateR2() {
+        this.r2 = ((-this.b + Math.sqrt(this.delta))/2*this.a);
+    }
+
+
+
 }
