abb6809550fc7322dee0713eff1e8cc62f4ebf16
Jose Almir <joseamfj@outlook.com>
2023-03-13 14:45:33 -0300

    Modify data type of the Xs and Ys variables and calculate x1 and x1 in the Pontos Constructor

--- Atividade6/Pontos.java
+++ Atividade6/Pontos.java
@@ -1,13 +1,17 @@
 package Atividade6;
 
 public class Pontos {
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
 
-    public Pontos(int a, int b, int c) {
-
+    public Pontos(int a, int b, int c, int y1, int y2) {
+        Bhaskara calculateRoots = new Bhaskara(a, b, c);
+        this.x1 = calculateRoots.getR1();
+        this.x2 = calculateRoots.getR2();
+        this.y1 = y1;
+        this.y2 = y2;
     }
     
 
