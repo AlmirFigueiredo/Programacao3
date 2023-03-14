b90a749d09e976cd51bc51e11d44901acf97d0f0
Jose Almir <joseamfj@outlook.com>
2023-03-14 12:21:42 -0300

    Create option to exit the program

--- Atividade6/Main.java
+++ Atividade6/Main.java
@@ -5,7 +5,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        while(true) {
+            
+
+            System.out.println("Deseja realizar outra comparacao?");
+            char option = input.next().toUpperCase().trim().charAt(0);
+            if(option == 'N') {
+                break;
+            }
+        }
 
 
         input.close();
