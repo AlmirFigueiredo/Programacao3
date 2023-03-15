951d0945d2481d20b290e618ff950a7b2373a9f3
Jose Almir <joseamfj@outlook.com>
2023-03-15 12:06:18 -0300

    Create method to calculate the distance among the circum point and another one

--- Atividade6/Points.java
+++ Atividade6/Points.java
@@ -11,19 +11,27 @@ public class Points {
         this.x = bhask.getR1();
         this.y = bhask.getR2();
         this.circum = circum;
+        this.distance = calculateDistance();
     }
     //Getters:
-        public Circle getCircum() {
-            return circum;
-        }
-        public double getDistance() {
-            return distance;
-        }
-        public double getX() {
-            return x;
-        }
-        public double getY() {
-            return y;
-        }
+    public Circle getCircum() {
+        return circum;
+    }
+    public double getDistance() {
+        return distance;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    private double calculateDistance() {
+        double x2 = this.circum.getXc();
+        double y2 = this.circum.getYc();
+        return (Math.sqrt(Math.pow((x2-this.x), 2) + Math.pow((y2-this.y), 2)));
+    }
+    
+
 }
 
