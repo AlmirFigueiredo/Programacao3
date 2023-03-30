1c70c7359631e96cbeba033eb2197b2f22d4f500
Jose Almir <joseamfj@outlook.com>
2023-03-30 12:01:37 -0300

    Create getters, setters and construtors

--- Atividade9/PacoteInterno/ContaBancaria.java
+++ Atividade9/PacoteInterno/ContaBancaria.java
@@ -1,8 +1,33 @@
 package Atividade9.PacoteInterno;
 
-public class ContaBancaria {
+public abstract class ContaBancaria {
     private String nomeDoCliente;
     private double saldo;
     private String numeroDaConta;
-    
+
+    public ContaBancaria(){}
+
+    //Getters:
+    public String getNomeDoCliente() {
+        return nomeDoCliente;
+    }
+    public String getNumeroDaConta() {
+        return numeroDaConta;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    //Setters:
+    public void setNomeDoCliente(String nomeDoCliente) {
+        this.nomeDoCliente = nomeDoCliente;
+    }
+    public void setNumeroDaConta(String numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
+
 }
