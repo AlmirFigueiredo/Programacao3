e8b1784809559c24657316f59c0a3f6a180cb8a4
Jose Almir <joseamfj@outlook.com>
2023-03-30 12:08:46 -0300

    Create getter and setter

--- Atividade9/PacoteInterno/ContaPoupanca.java
+++ Atividade9/PacoteInterno/ContaPoupanca.java
@@ -4,6 +4,13 @@ public class ContaPoupanca extends ContaBancaria {
     private int diaDeRendimento;
     public ContaPoupanca(String nome, double saldo, String numeroDaConta) {
         super(nome, saldo, numeroDaConta);
-        
     }
+    //Getter and Setter:
+    public int getDiaDeRendimento() {
+        return diaDeRendimento;
+    }
+    public void setDiaDeRendimento(int diaDeRendimento) {
+        this.diaDeRendimento = diaDeRendimento;
+    }
+    
 }
