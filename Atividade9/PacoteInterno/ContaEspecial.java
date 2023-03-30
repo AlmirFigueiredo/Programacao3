7104c197971ee3c1402355b207bd8b8bd759166d
Jose Almir <joseamfj@outlook.com>
2023-03-30 12:28:26 -0300

    Create getter and set in the class ContaEspecial

--- Atividade9/PacoteInterno/ContaEspecial.java
+++ Atividade9/PacoteInterno/ContaEspecial.java
@@ -1,5 +1,17 @@
 package Atividade9.PacoteInterno;
 
 public class ContaEspecial extends ContaBancaria {
+    private double limite;
+    public ContaEspecial(String nome, String numeroDaConta, double saldo, double limite) {
+        super(nome, saldo, numeroDaConta);
+        this.limite = limite;
+    }
+    //Getters and setters:
+    public double getLimite() {
+        return limite;
+    }
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
     
 }
