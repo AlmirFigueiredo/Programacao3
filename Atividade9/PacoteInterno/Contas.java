b9cc49cd3c60b6482d970a6ed9e6aa0f9cdb70de
Jose Almir <joseamfj@outlook.com>
2023-03-30 20:08:54 -0300

    Create methods to get the money in the accounts

--- Atividade9/PacoteInterno/Contas.java
+++ Atividade9/PacoteInterno/Contas.java
@@ -12,30 +12,47 @@ public class Contas {
         this.listaDeContasEspeciais = new ContaEspecial[this.quantidadeDeContasEspeciais];
         this.listaDeContasPoupanca = new ContaPoupanca[this.quantidadeDeContasPoupanca];
     }
+
     public void IncluirContaPoupanca(ContaPoupanca conta) {
-        ContaPoupanca[] temp = new ContaPoupanca[this.quantidadeDeContasPoupanca+1];
+        ContaPoupanca[] temp = new ContaPoupanca[this.quantidadeDeContasPoupanca + 1];
         int i;
-        for(i = 0; i < this.quantidadeDeContasPoupanca; i++) {
+        for (i = 0; i < this.quantidadeDeContasPoupanca; i++) {
             temp[i] = this.listaDeContasPoupanca[i];
         }
         temp[i] = conta;
         this.listaDeContasPoupanca = temp;
     }
+
     public void IncluirContaEspecial(ContaEspecial conta) {
         ContaEspecial[] temp = new ContaEspecial[this.quantidadeDeContasEspeciais + 1];
         int i;
-        for(i = 0; i < this.quantidadeDeContasEspeciais; i++) {
+        for (i = 0; i < this.quantidadeDeContasEspeciais; i++) {
             temp[i] = this.listaDeContasEspeciais[i];
         }
         temp[i] = conta;
         this.listaDeContasEspeciais = temp;
     }
-    public void sacarContaPoupanca(double valor, ContaPoupanca poupanca) {
-        for(int i = 0; i < this.quantidadeDeContasPoupanca; i++) {
-            this.listaDeContasPoupanca[i].equals(poupanca);
-        }
-    }
-    
 
+    public void sacarContaPoupanca(double valor, ContaPoupanca poupanca) {
+        for (int i = 0; i < this.quantidadeDeContasPoupanca; i++) {
+            if(this.listaDeContasPoupanca[i].equals(poupanca)) {
+                double saldoAnterior = this.listaDeContasPoupanca[i].getSaldo();
+                this.listaDeContasPoupanca[i].setSaldo(saldoAnterior-valor);
+                return;
+            }
+        }
+        System.out.println("Conta nao encontrada!");
+    }
+
+    public void sacarContaEspecial(double valor, ContaEspecial conta) {
+        for(int i = 0; i < this.quantidadeDeContasEspeciais; i++) {
+            if(this.listaDeContasEspeciais[i].equals(conta)) {
+                double saldoAnterior = this.listaDeContasEspeciais[i].getSaldo();
+                this.listaDeContasPoupanca[i].setSaldo(saldoAnterior-valor);
+                return;
+            }
+        }
+        System.out.println("Conta nao encontrada!");
+    }
 
 }
